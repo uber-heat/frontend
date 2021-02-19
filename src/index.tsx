ad6d8b4fa2c4f7a8cb1fc23cd87168fddfc4ef8f
Miki Namsrai <miki.namsrai@gmail.com>
2021-02-19 11:42:46 +0100

    add tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/app';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/output.css'
 
 ReactDOM.render(
   <React.StrictMode>
