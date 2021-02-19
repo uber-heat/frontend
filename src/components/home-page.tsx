b6f011c72ed61426ad8b4a13f7b6d17cdff945cd
rocknologie <rocknologie@hotmail.fr>
2021-02-19 12:30:08 +0100

    Init homepage

---
+++ src/components/home-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Home page component.
+ */
+export const HomePage: React.FC = () => (
+  <div className="mt-3">
+    <div className="container mx-auto">
+     <p>Slt</p>
+    </div>
+  </div>
+)
